8739f5b9bd6570367131f877cf3bfd693e28e5ec
lee <lee88@naokou.com>
2022-03-11 12:10:25 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Lishiyu!");
         System.out.println("hello,Lishiyu2!");
-        System.out.println("hello,Lishiyu2!");
+        System.out.println("hello,Lishiyu3!");
+        System.out.println("hello,Lishiyu4!");
     }
 }

027026fefd1ede5e80321ba75853960780e49017
lee <lee88@naokou.com>
2022-03-11 11:49:32 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Lishiyu!");
         System.out.println("hello,Lishiyu2!");
+        System.out.println("hello,Lishiyu2!");
     }
 }
